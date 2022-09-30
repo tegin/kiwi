f04520b9011f3bca57b248401b5a8a8201a63aca
Núria Sancho <nuria.sancho@creublanca.es>
2022-09-30 11:42:26 +0200

    [ADD] queue_management: new model queue_token_location_action

--- queue_management/models/queue_token_location_action.py
+++ queue_management/models/queue_token_location_action.py
@@ -1,17 +1,18 @@
 # Copyright 2022 CreuBlanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class QueueTokenLocationAction(models.Model):
 
     _name = "queue.token.location.action"
-    _description = "Queue_token_location_action"  # TODO
+    _description = "Queue_token_location_action"
     _log_access = False
+    _order = "date desc"
 
     token_id = fields.Many2one("queue.token", required=True, readonly=True)
-    token_location = fields.Many2one("queue.token.location")
+    token_location_id = fields.Many2one("queue.token.location")
     location_id = fields.Many2one("queue.location", readonly=True)
     user_id = fields.Many2one("res.users", readonly=True)
     date = fields.Datetime(readonly=True)
@@ -21,7 +22,18 @@ class QueueTokenLocationAction(models.Model):
             ("assign", "Assign"),
             ("leave", "Leave"),
             ("reopen", "Reopen"),
-            ("back_to_draft" , "Back to draft")
-
+            ("back_to_draft", "Back to draft"),
         ]
     )
+
+    def name_get(self):
+        result = []
+        for record in self:
+            result.append(
+                (
+                    record.id,
+                    "%s-%s %s"
+                    % (record.token_id.name, record.location_id.name, record.action),
+                )
+            )
+        return result
