e6fd4af41cfc587a41eb1f123552ec6db4135db9
Núria Sancho <nuria.sancho@creublanca.es>
2022-10-03 12:38:37 +0200

    [IMP] queue_management: Add purgable functionality

--- queue_management/models/queue_token_location_action.py
+++ queue_management/models/queue_token_location_action.py
@@ -1,7 +1,12 @@
 # Copyright 2022 CreuBlanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+import logging
+from datetime import timedelta
+
+from odoo import api, fields, models, registry
+
+_logger = logging.getLogger(__name__)
 
 
 class QueueTokenLocationAction(models.Model):
@@ -37,3 +42,30 @@ class QueueTokenLocationAction(models.Model):
                 )
             )
         return result
+
+    @api.model
+    def autovacuum(self, days=30):
+        self.search(self._search_autovacuum_domain(days=days)).batch_unlink()
+
+    def _search_autovacuum_domain(self, days=30):
+        return [(("date", "<", fields.Datetime.now() + timedelta(days=-days)))]
+
+    def batch_unlink(self):
+        with api.Environment.manage():
+            with registry(self.env.cr.dbname).cursor() as new_cr:
+                new_env = api.Environment(new_cr, self.env.uid, self.env.context)
+                try:
+                    while self:
+                        batch_delete = self[0:1000]
+                        self -= batch_delete
+                        # do not attach new env to self because it may be
+                        # huge, and the cache is cleaned after each unlink
+                        # so we do not want to much record is the env in
+                        # which we call unlink because odoo would prefetch
+                        # fields, cleared right after.
+                        batch_delete.with_env(new_env).unlink()
+                        new_env.cr.commit()
+                except Exception as e:
+                    _logger.exception(
+                        "Failed to delete records : {} - {}".format(self._name, str(e))
+                    )
--- queue_management/tests/test_token_location_action.py
+++ queue_management/tests/test_token_location_action.py
@@ -1,13 +1,18 @@
 # Copyright 2022 CreuBlanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+import freezegun
 
+from odoo import api
 from odoo.tests.common import TransactionCase
 
 
 class TestTokenLocationAction(TransactionCase):
     def setUp(self):
         super().setUp()
-
+        self.registry.enter_test_mode(self.env.cr)
+        self.env = api.Environment(
+            self.registry.test_cr, self.env.uid, self.env.context
+        )
         self.location_1 = self.env["queue.location"].create(
             {"name": "L1", "group_ids": []}
         )
@@ -24,6 +29,10 @@ class TestTokenLocationAction(TransactionCase):
         self.token_l2 = self.env["queue.token"].create(
             {"location_ids": [(0, 0, {"location_id": self.location_2.id})]}
         )
+
+    def tearDown(self):
+        self.registry.leave_test_mode()
+        super().tearDown()
 
     def test_token_location_back_to_draft_action(self):
         self.token_l1.location_ids.with_context(
@@ -104,3 +113,14 @@ class TestTokenLocationAction(TransactionCase):
         self.assertFalse(last_row.location_id)
         self.assertTrue(last_row.date)
         self.assertEqual(last_row.user_id.id, self.env.user.id)
+
+    def test_token_location_action_delete(self):
+        with freezegun.freeze_time("2020-01-01"):
+            self.token_l1.location_ids.with_context(
+                location_id=self.location_1.id
+            ).action_cancel()
+        action = self.token_l1.action_view_log()
+        self.assertTrue(self.env[action["res_model"]].search(action["domain"]))
+        self.env["queue.token.location.action"].autovacuum()
+        self.env[action["res_model"]].refresh()
+        self.assertFalse(self.env[action["res_model"]].search(action["domain"]))
