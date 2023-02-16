cdc6d4182c5efe5028422ae41a1d31742835fa08
Kevin Luna <kevin.luna@creublanca.es>
2023-02-16 12:21:13 +0100

    [ADD] queue_management_planning

---
+++ queue_management_planning/models/queue_location.py
@@ -0,0 +1,27 @@
+# Copyright 2023 CreuBlanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class QueueLocation(models.Model):
+
+    _inherit = "queue.location"
+
+    def _get_token_location_domain(self):
+        domain = super()._get_token_location_domain()
+        # domain.append(("token_id.arrival_date", "!=", False))
+        return domain
+
+    def _get_token_location_order(self):
+        order = super()._get_token_location_order()
+        return "planning_date asc, " + order
+
+    def access_location(self):
+        self.ensure_one()
+        action = self.env.ref(
+            "queue_management_planning.queue_token_location_calendar_act_window"
+        ).read()[0]
+        # TODO: write the domain....
+        # action["domain"] = self._get_token_location_domain()
+        return action
