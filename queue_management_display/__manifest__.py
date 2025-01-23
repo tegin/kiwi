3af8dc67d14c9d9cf605b319032d8d9f72865a99
Luis Rodriguez <luis.rodriguez@dixmit.com>
2025-01-23 09:11:59 +0100

    [MIG] queue_management_display: Migration to 16.0

--- queue_management_display/__manifest__.py
+++ queue_management_display/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Queue Management Display",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/tegin/kiwi",
     "author": "CreuBlanca",
@@ -13,8 +13,13 @@
         "views/queue_location.xml",
         "security/ir.model.access.csv",
         "views/queue_display.xml",
-        "templates/assets.xml",
     ],
-    "qweb": ["static/src/xml/queue_management.xml"],
+    # "qweb": ["static/src/xml/queue_management.xml"],
     "demo": ["demo/data.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "/queue_management_display/static/src/components/**/*.js",
+            "/queue_management_display/static/src/components/**/*.xml",
+        ]
+    },
 }
